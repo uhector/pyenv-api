db7012a90a385fb962d4668fab21e0646ddce9c3
Hector Ulacio <hectorulacior@gmail.com>
2020-06-08 22:06:32 -0400

    Add 'commands' import

--- src/pyenv_api/api.py
+++ src/pyenv_api/api.py
@@ -1,3 +1,17 @@
+from .commands import (
+    FORCE,
+    GLOBAL,
+    INSTALL,
+    LIST,
+    PYENV,
+    ROOT,
+    UNINSTALL,
+    VERSION,
+    VERSIONS,
+    VERBOSE
+)
+
+
 class PyenvAPI(object):
     """The PyenvAPI class implements an interface that lets it interact
     with pyenv through subprocess module.
