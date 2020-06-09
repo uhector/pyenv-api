a42ddce8f65476afdb6d14c89ab223a57ed505b3
Hector Ulacio <hectorulacior@gmail.com>
2020-06-09 02:11:58 -0400

    Add inline comments

--- src/pyenv_api/commands.py
+++ src/pyenv_api/commands.py
@@ -1,19 +1,19 @@
-"""Like git, the pyenv command delegates to subcommands based on its
+"""The pyenv command delegates to subcommands based on its
 first argument.
 """
 
 # Main command
-PYENV = 'pyenv'
+PYENV = 'pyenv' # Lists all available pyenv commands
 
 # Subcommands
-GLOBAL   = 'global'
-INSTALL  = 'install'
-ROOT = 'root' 
-UNINSTALL = 'uninstall'
-VERSION = 'version'
-VERSIONS = 'versions'
+GLOBAL   = 'global' # Sets the global version of Python
+INSTALL  = 'install' # Install a Python version
+ROOT = 'root' # Display the pyenv's root directory
+UNINSTALL = 'uninstall' # Uninstall a specific Python version
+VERSION = 'version' # Displays the currently active Python version
+VERSIONS = 'versions' # Lists all Python versions known to pyenv
 
 # Options
-FORCE = '--force'
-LIST = '--list'
-VERBOSE = '--verbose'
+FORCE = '--force' # Install even if the version appears to be installed already
+LIST = '--list' # List all available versions
+VERBOSE = '--verbose' # Verbose mode: print compilation status to stdout
